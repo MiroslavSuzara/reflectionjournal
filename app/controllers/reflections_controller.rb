8e43a79d4112e68d6702e7cfb69a678b9ff2aae9
Miroslav Suzara <miroslav.suzara@gmail.com>
2015-03-02 14:39:47 -0800

    Added sign-in/sign-out to view pages.

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,5 +1,6 @@
 class ReflectionsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @reflections = Reflection.all.order("created_at DESC")
   end
